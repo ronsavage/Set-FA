a27f5894625af6913c050317cef318debacaddf4
Ron Savage <ron@savage.net.au>
2011-06-27 12:17:13 +1000

    When stuck in loop, print next 5 chars in hex

--- lib/Set/FA/Element.pm
+++ lib/Set/FA/Element.pm
@@ -48,7 +48,7 @@ sub advance
 
 		if (length($output) >= length($input) )
 		{
-			my($prefix) = '<', join('> <' . map{sprintf('%02x', $_)} split(//, substr($input, 0, 5) ) ) . '>';
+			my($prefix) = '<', join('> <' . map{sprintf '%02x', ord $_} split(//, substr($input, 0, 5) ) ) . '>';
 
 			$self -> log( ($self -> die_on_loop ? 'error' : 'warning') => "State: '" . $self -> current . "' is not consuming input. Next 5 chars: $prefix");
 		}
